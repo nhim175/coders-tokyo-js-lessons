dd2e611f0ee42a42ef99461e1972bedbb5e08320
Thinh Pham <nhim175@gmail.com>
2018-07-05 12:04:14 +0900

    JSON methods

---
+++ 32-JSON/index.js
@@ -0,0 +1,14 @@
+// JSON object
+// 1. stringify - Convert an object to a JSON string
+// 2. parse - Convert a JSON string to an object
+var myDog = { name: 'Milu', age: 1, dead: false };
+var myDogString = JSON.stringify(myDog);
+
+var myCatString = '{"name": "Tom", "age": 2, "dead": true}';
+var myCat = JSON.parse(myCatString);
+console.log(myCat);
+
+// Exercise
+// 1. Show all students
+// 2. Create a new student
+// 3. Save & Exit
