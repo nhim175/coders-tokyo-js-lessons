ded34479d7fd1b7c008899dd619ece0cf37f66ab
Thinh Pham <nhim175@gmail.com>
2018-07-07 13:44:33 +0900

    While loops

---
+++ 37-while-loop/index.js
@@ -0,0 +1,28 @@
+// while, do ... while loops
+//
+// Syntax
+//
+// while (condition) {
+//  // do something
+// }
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+var i = 1;
+
+// while(i <= 10) {
+//   console.log(i);
+//   ++i;
+// }
+
+// console.log('xxx', i);
+
+// Syntax
+// do {
+//   // do something
+// } while(condition)
+do {
+  console.log(i);
+  ++i;
+} while(i <= 10)
